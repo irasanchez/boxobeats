755577980423bd9bf6f16afe04a0ded7ca57c2eb
iradsanchez <ira.d.sanchez@gmail.com>
2019-08-08 09:48:41 -0700

    Add Conditional Rendering of Beats Based on Button Type
    If the button shows the play icon, hide the beats. If the button shows the pause icon, show the beats.

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ function App() {
   };
   return (
     <div className="App">
-      <RandomBeatList />
+      {type === "playCircle" ? null : <RandomBeatList />}
       <MorphIcon type={type} onClick={playButtonHandler} />
     </div>
   );

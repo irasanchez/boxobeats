711a1c42f0318012d2397240ff9d524068a030c4
Ira <ira.d.sanchez@gmail.com>
2019-11-08 09:23:03 -0700

    Add notes for future changes, minor edits to CSS.

---
+++ app/src/components/CreateView.js
@@ -0,0 +1,6 @@
+/*
+  //TODO:
+    - "Now try to combine"
+    - Sound 1 and Sound 2 rendered
+    - make each one refreshable for the user to be able to change things.
+*/
--- app/src/components/Progress/ProgressView.js
+++ app/src/components/Progress/ProgressView.js
@@ -1,7 +1,12 @@
 import React from "react";
 import style from "./progress.module.css";
 /* //TODO:
-    Look up how to make the progress percentage dynamic, so that I can make each one specific to each sound */
+    - Look up how to make the progress percentage dynamic, so that I can make each one specific to each sound 
+    - Separate out the li's into their own components. 
+    - Add more data like "exit" and "origin" to data set to be able to render those notes here.
+    - Render those notes here.
+    - Watch on YouTube links open in new window
+*/
 
 const ProgressView = props => {
   const { beats } = props;

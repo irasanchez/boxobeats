fb122e4affa441d1ca3ae93914e4bdb045b192a6
Ira <ira.d.sanchez@gmail.com>
2019-11-09 10:48:25 -0700

    Add Emotion; Remove Unnecessary Dependencies

--- app/src/components/BottomNav/BottomNav.js
+++ app/src/components/BottomNav/BottomNav.js
@@ -3,12 +3,21 @@ import { FaRegMap, FaRegLightbulb, FaHammer, FaRegEye } from "react-icons/fa";
 import style from "./bottomNav.module.css";
 
 const BottomNav = props => {
-  const { bottomNav, icon1, icon2, icon3, icon4, icons } = style;
+  const {
+    bottomNav,
+    icon1,
+    icon2,
+    icon3,
+    icon4,
+    icons,
+    metronomeContainer
+  } = style;
   return (
     <footer className={bottomNav}>
       <nav>
         <FaRegMap id={icon1} className={icons} />
         <FaRegLightbulb id={icon2} className={icons} />
+        <div className={metronomeContainer}></div>
         <FaHammer id={icon3} className={icons} />
         <FaRegEye id={icon4} className={icons} />
       </nav>

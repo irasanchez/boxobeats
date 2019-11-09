162adc3ec0c0b3d1ea7d2da406d4c65a3fa8b5ca
Ira <ira.d.sanchez@gmail.com>
2019-11-09 01:09:33 -0700

    style icons on bottom navbar

--- app/src/components/BottomNav/BottomNav.js
+++ app/src/components/BottomNav/BottomNav.js
@@ -3,14 +3,14 @@ import { FaRegMap, FaRegLightbulb, FaHammer, FaRegEye } from "react-icons/fa";
 import style from "./bottomNav.module.css";
 
 const BottomNav = props => {
-  const { bottomNav } = style;
+  const { bottomNav, icon1, icon2, icon3, icon4, icons } = style;
   return (
     <footer className={bottomNav}>
       <nav>
-        <FaRegMap />
-        <FaRegLightbulb />
-        <FaHammer />
-        <FaRegEye />
+        <FaRegMap id={icon1} className={icons} />
+        <FaRegLightbulb id={icon2} className={icons} />
+        <FaHammer id={icon3} className={icons} />
+        <FaRegEye id={icon4} className={icons} />
       </nav>
     </footer>
   );

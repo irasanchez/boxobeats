5f049ac11d6d2dda2ace68c0e726ddda76418743
Ira <ira.d.sanchez@gmail.com>
2019-11-09 01:29:58 -0700

    begin Building Create View
    - Add refresh icons to switch sounds out
    - Add plus icon instead of using the word and
    - Add hardcoded data to sample until I build an api
    - Add additional information text, in case the simple instructions are confusing

--- app/src/components/Create/CreateView.js
+++ app/src/components/Create/CreateView.js
@@ -5,11 +5,33 @@
 */
 
 import React from "react";
+import { FiRefreshCcw, FiPlusSquare } from "react-icons/fi";
+import style from "./create.module.css";
 
 const CreateView = props => {
+  const { beats } = props;
+  const { comboContainer, soundOption } = style;
   return (
     <section className="create-view">
       <p>Try to combine</p>
+      <div className={comboContainer}>
+        <FiRefreshCcw />
+        <div className={soundOption}>{beats[0].name}</div>
+        <FiPlusSquare />
+        <div className={soundOption}>{beats[1].name}</div>
+        <FiRefreshCcw />
+      </div>
+      <div>
+        <p>Information:</p>
+        <p>
+          Many of the more complex sounds one can make come from combining 2 or
+          more sounds and making them at the same time.{" "}
+        </p>
+        <p>
+          It takes a lot of practice to figure out your body positioning and
+          timing sometimes. So, don't give up if you don't get it right away.
+        </p>
+      </div>
     </section>
   );
 };

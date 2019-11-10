8ab9310214c22a901cdc44b4d6543b12f43329b1
Ira <ira.d.sanchez@gmail.com>
2019-11-10 11:46:54 -0700

    Add Notes to cssVariables, Fix Colors. - Don't try to style things when you have a dark mode extension on!

--- app/src/assets/cssVariables.js
+++ app/src/assets/cssVariables.js
@@ -1,18 +1,22 @@
 /** @jsx jsx */
 import { css } from "@emotion/core";
 
-/* Coolors Exported Palette - coolors.co/462434-e41b21-14808a-e8e8e8-151515 
-inspired by https://dribbble.com/shots/3771065-Beatbox-Faces
-*/
-export const mainBg = `rgba(70, 36, 52, 1)`;
-export const textColor = `rgba(232, 232, 232, 1)`;
-export const secondaryBg = `rgba(21, 21, 21, 1)`;
+// COLORS
+/* Inspired by https://dribbble.com/shots/3771065-Beatbox-Faces
+ */
 
+//Licorice (black)
+export const mainBg = `rgba(21, 21, 21, 1)`;
+//Silver
+export const textColor = `#c0c0c0`;
+//Old Burgundy
+export const secondaryBg = `rgba(70, 36, 52, 1)`;
+//Metallic Seaweed
 export const accentColorBlue = `rgba(20, 128, 138, 1)`;
-
+//Shamrock Green
 export const accentColorGreen = `rgba(0, 158, 96, 1)`;
-
-export const highlight = `rgba(242, 229, 204, 1)`;
+// white-like color, idk the name
+export const highlight = `rgba(232, 232, 232, 1)`;
 
 //POSITIONING AND ALIGNMENT
 export const center = `justify-content: center; align-items: center;`;
@@ -24,6 +28,7 @@ export const boxShadow = `-webkit-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.75
 -moz-box-shadow:0px 5px 0px rgba(0, 0, 0, 0.75);
 box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.75);`;
 
+//DYNAMISM
 export const hideThisElement = css`
   display: none;
 `;

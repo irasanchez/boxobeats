7dd8e31dec459dd1aa0327f58b9df157f00b514d
Ira <ira.d.sanchez@gmail.com>
2019-12-07 17:25:17 -0700

    Getting ready for implementing re-organized plans

--- app/src/App.js
+++ app/src/App.js
@@ -8,87 +8,12 @@ import { Route, withRouter } from "react-router-dom";
 //FIXME: hardcoded data to later be removed after I make some sort of backend
 import beatsData from "./assets/beatsData.json";
 
-//component imports
-import HomeView from "./components/Home/HomeView";
-import ProgressView from "./components/Progress/ProgressView";
-import CreateView from "./components/Create/CreateView";
-import FocusView from "./components/Focus/FocusView";
-import ImprovView from "./components/Improv/ImprovView";
-import BottomNav from "./components/BottomNav/BottomNav";
-
-//styles
-import { app, global } from "./AppStyles.js";
-
-//NOTE: beatBud is the mascot for the app
-
 const App = props => {
   //grab data from json file and set it to state
   //FIXME: Must get data dynamically using axios
   const [beats] = useState(beatsData.beats);
 
-  return (
-    <div css={app}>
-      <Global styles={global} />
-      {/* HOME VIEW */}
-      <Route
-        exact
-        path="/"
-        render={props => (
-          <HomeView history={props.history} location={props.location} />
-        )}
-      />
-
-      {/* PROGRESS VIEW */}
-      <Route
-        path="/progress"
-        render={props => (
-          <ProgressView
-            beats={beats}
-            history={props.history}
-            location={props.location}
-          />
-        )}
-      />
-
-      {/* CREATE VIEW */}
-      <Route
-        path="/create"
-        render={() => (
-          <CreateView
-            beats={beats}
-            history={props.history}
-            location={props.location}
-          />
-        )}
-      />
-
-      {/* IMPROV VIEW */}
-      <Route
-        path="/improv"
-        render={props => (
-          <ImprovView
-            beats={beats}
-            history={props.history}
-            location={props.location}
-          />
-        )}
-      />
-
-      {/* FOCUS VIEW */}
-      <Route
-        path="/focus"
-        render={() => (
-          <FocusView
-            beats={beats}
-            history={props.history}
-            location={props.location}
-          />
-        )}
-      />
-
-      {props.location.pathname !== "/" && <BottomNav />}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default withRouter(App); //withRouter gives App access to location prop. if <Component /> is not inside of a <Route /> then it doesn't have access to location, match, etc.

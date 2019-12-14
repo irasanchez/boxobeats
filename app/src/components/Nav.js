fa6b846a9d805bac4c50ce5bddcce4f8d10aaf2e
Ira <ira.d.sanchez@gmail.com>
2019-12-13 21:49:05 -0700

    Add Icons and NavLinks to Nav

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,7 +1,37 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 
-const Nav = () => {
-  return <div></div>;
+import { NavLink } from "react-router-dom";
+
+// icons
+// sizes have to be put on the size prop. default is 1em. rem doesn't appear to work.
+import { FaRegMap, FaRegLightbulb, FaHammer, FaRegEye } from "react-icons/fa";
+import { GiMetronome } from "react-icons/gi";
+
+const Nav = props => {
+  const sideIconSize = "2em";
+  return (
+    <footer>
+      <nav>
+        <NavLink to="/progress">
+          <FaRegMap />
+        </NavLink>
+        <NavLink to="/improv">
+          <FaRegLightbulb />
+        </NavLink>
+
+        <GiMetronome />
+
+        <NavLink to="/create">
+          <FaHammer />
+        </NavLink>
+
+        <NavLink to="/focus">
+          <FaRegEye />
+        </NavLink>
+      </nav>
+    </footer>
+  );
 };
 
 export default Nav;

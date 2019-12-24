5182f3315d33d0b68b9c5f21bfa9ead218ac6b86
Ira <ira.d.sanchez@gmail.com>
2019-12-24 15:21:04 -0700

    Add Get Random Sound Funtionality and SoundCard to Focus View

--- app/src/components/Views/Focus.js
+++ app/src/components/Views/Focus.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import SoundCard from "../SoundCard/SoundCard";
 const Focus = ({ beats }) => {
-  return <main></main>;
+  const beat = beats.sort(() => 0.5 - Math.random()).slice(0, 1);
+
+  return (
+    <main>
+      <SoundCard beat={beat} />
+    </main>
+  );
 };
 
 export default Focus;

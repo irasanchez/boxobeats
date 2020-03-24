bb9e28dce608ada2c967162d67f94302f6cb1f13
Ira <ira.d.sanchez@gmail.com>
2020-03-23 20:46:21 -0700

    Add basic test for App

--- app/src/tests/SoundList.test.js
+++ app/src/tests/SoundList.test.js
@@ -3,5 +3,5 @@ import SoundList from "../components/SoundList";
 import { render } from "@testing-library/react";
 
 test("renders", () => {
-  const { queryByTestId } = render(<SoundList beats={[]} />);
+  render(<SoundList beats={[]} />);
 });

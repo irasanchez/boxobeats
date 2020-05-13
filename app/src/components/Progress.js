90210bab51261721df853c8e41856fee8e3fa6d4
Ira <ira.d.sanchez@gmail.com>
2020-05-13 06:52:04 -0700

    Add conditional rendering to sound list

--- app/src/components/Progress.js
+++ app/src/components/Progress.js
@@ -3,34 +3,38 @@ import { Grid, Typography, Paper } from "@material-ui/core";
 import { Route } from "react-router-dom";
 import Sound from "./Sound";
 import SoundList from "./SoundList";
-const Progress = props => {
+const Progress = (props) => {
   const styles = {
     View: {
       height: "80vh",
       maxHeight: "80vh",
-      width: "100%"
+      width: "100%",
     },
     Panels: {
       height: "100%",
       maxHeight: "100%",
       overflowY: "auto",
       display: "flex",
-      flexDirection: "column"
+      flexDirection: "column",
     },
     Paper: {
       height: "100%",
-      width: "100%"
+      width: "100%",
       // padding: "0 5%",
-    }
+    },
   };
 
   return (
     <Grid container style={styles.View}>
-      <Grid item sm={5} style={styles.Panels}>
-        <Paper>
-          <SoundList beats={props.beats} setCurrent={props.setCurrent} />
-        </Paper>
-      </Grid>
+      {props.beats.length ? (
+        // if I just check props.beats, the component still renders even if empty
+        // props.beats.length && <Component /> => results in the length being rendered, even if 0
+        <Grid item sm={5} style={styles.Panels}>
+          <Paper>
+            <SoundList beats={props.beats} setCurrent={props.setCurrent} />
+          </Paper>
+        </Grid>
+      ) : null}
       <Grid item container sm={7} style={styles.Panels}>
         <Route exact path="/progress">
           <Typography variant="h3">Welcome</Typography>

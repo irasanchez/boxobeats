0135b2fb44c20ff30448aa995300eeb8e1f4c57d
Ira <ira.d.sanchez@gmail.com>
2020-06-23 10:35:07 -0700

    Add button linking to practice view

--- app/src/experiment/components/Sounds.js
+++ app/src/experiment/components/Sounds.js
@@ -3,17 +3,42 @@ import React from "react";
 //Components
 import Sound from "./Sound";
 //MUI
-import { List } from "@material-ui/core";
+import { List, Fab } from "@material-ui/core";
+import NavigateNextSharpIcon from "@material-ui/icons/NavigateNextSharp";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 //Redux
 import { connect } from "react-redux";
 import { getSounds, togglePractice } from "../../actions/actions";
+//React Router
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
 const Sounds = (props) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
   return (
     <List>
       {props.sounds.map((sound) => {
         return <Sound name={sound.name} />;
       })}
+      <Link>
+        <Fab
+          color="secondary"
+          aria-label="practice"
+          size="large"
+          className={classes.fab}
+        >
+          <NavigateNextSharpIcon />
+        </Fab>
+      </Link>
     </List>
   );
 };

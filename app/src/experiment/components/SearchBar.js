1d8926da68ffb741915a1df57b7d055abf823c54
Ira <ira.d.sanchez@gmail.com>
2020-06-23 13:38:03 -0700

    fix search bar styling fdor mobile

--- app/src/experiment/components/SearchBar.js
+++ app/src/experiment/components/SearchBar.js
@@ -6,12 +6,57 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
+import { fade, makeStyles } from "@material-ui/core/styles";
 import { gray } from "../../assets/colors";
 //Redux
 import { connect } from "react-redux";
 import { setFilter } from "../../actions/actions";
 
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputInput: {
+    color: "inherit",
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 const SearchBar = (props) => {
+  const classes = useStyles();
+
   const [searchQuery, setSearchQuery] = useState("");
 
   const changeHandler = (event) => {
@@ -23,14 +68,15 @@ const SearchBar = (props) => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <div>
-          <div>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
             <SearchIcon />
           </div>
           <InputBase
             placeholder="Search…"
             inputProps={{ "aria-label": "search" }}
             onChange={changeHandler}
+            className={classes.inputInput}
           />
         </div>
       </Toolbar>

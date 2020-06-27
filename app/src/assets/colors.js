4a11e23f2e71991858844d36234d096a925301be
Ira <ira.d.sanchez@gmail.com>
2020-06-27 13:55:08 -0700

    add light and dark color codes

--- app/src/assets/colors.js
+++ app/src/assets/colors.js
@@ -1,21 +1,32 @@
 module.exports = {
   black: {
+    light: "#434343",
     main: "#151515",
+    dark: "#0e0e0e",
   },
   blue: {
+    light: "#4399a1",
     main: "#14808A",
+    dark: "#0e5960",
   },
   red: {
+    light: "#dd474c",
     main: "#D51A20",
-    dark: "#911216",
+    dark: "#951216",
   },
   gray: {
+    light: "#afaeaf",
     main: "#9B9A9B",
+    dark: "#6c6b6c",
   },
   purple: {
+    light: "#8d7d97",
     main: "#715D7D",
+    dark: "#4f4157",
   },
   brown: {
+    light: "#c3816f",
     main: "#B4624C",
+    dark: "#7d4435",
   },
 };
